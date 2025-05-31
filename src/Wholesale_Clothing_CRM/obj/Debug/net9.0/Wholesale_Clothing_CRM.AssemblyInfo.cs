ae9e06db8469026b6150bbfd5d5c0c506cf5e6fc
Sherzod Jumayev <jumayevsherzod304@gmail.com>
2025-06-01 00:57:55 +0500

    Second commit

--- src/Wholesale_Clothing_CRM/obj/Debug/net9.0/Wholesale_Clothing_CRM.AssemblyInfo.cs
+++ src/Wholesale_Clothing_CRM/obj/Debug/net9.0/Wholesale_Clothing_CRM.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25b989003ea699e93b16310fde03f193500017c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
