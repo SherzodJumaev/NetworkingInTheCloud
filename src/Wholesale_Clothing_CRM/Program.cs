16a5410da4ec64471fbaf475b20c8884762eb9b6
Sherzod Jumayev <jumayevsherzod304@gmail.com>
2025-06-01 14:48:29 +0500

    some changes 3

--- src/Wholesale_Clothing_CRM/Program.cs
+++ src/Wholesale_Clothing_CRM/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 
--- src/Wholesale_Clothing_CRM/obj/Debug/net9.0/Wholesale_Clothing_CRM.AssemblyInfo.cs
+++ src/Wholesale_Clothing_CRM/obj/Debug/net9.0/Wholesale_Clothing_CRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0e9fc2fb34f3cf39b0c23e68b43e84c01ea056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e02b8102c41ce69d0d8b2630cc59e14fbd4e7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wholesale_Clothing_CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
